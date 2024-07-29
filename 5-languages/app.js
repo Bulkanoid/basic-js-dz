b925ffdb8a03cfc6d340fec0186a072a94aa7817
Paul bulkanoid <bulkanoid@gmail.com>
2024-07-29 20:34:19 +1000

    languages

---
+++ 5-languages/app.js
@@ -0,0 +1,23 @@
+let req = prompt(`Укажите язык: 
+(пример: en, ru, de)`);
+
+if (req.length > 2) {
+  req = 'error';
+}
+
+switch (req) {
+  case 'error':
+    alert('Enter two letter country code');
+    break;
+  case 'en':
+    alert('Hello in English');
+    break;
+  case 'de':
+    alert('Hello in Germanskiy');
+  case 'ru':
+  case 'pl':
+    alert('Hello in Russian');
+    break;
+  default:
+    alert('Enter existing language');
+}
