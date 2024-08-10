d1f8fdf3f64966d5a235194c17a4787dc339466a
Paul bulkanoid <bulkanoid@gmail.com>
2024-08-10 17:44:20 +1000

    Внесены правки по результатам проверки дз
    Добавил проверку на високосный год, скорректировал порядок дат, вывод дат

--- 11-date-array/app.js
+++ 11-date-array/app.js
@@ -1,31 +1,41 @@
 const isDayOrMonth = (data) => {
-  const firstNum = Number(data[0]);
-  const secondNum = Number(data[1]);
+  const firstNum = parseInt(data[0]);
+  const secondNum = parseInt(data[1]);
 
   if (
     isNaN(firstNum) ||
     isNaN(secondNum) ||
-    firstNum < 0 ||
-    secondNum < 0 ||
+    firstNum <= 0 ||
+    secondNum <= 0 ||
     firstNum > 31 ||
     secondNum > 31
   ) {
     return false;
   }
 
-  if (firstNum <= 12 && secondNum <= 12) {
-    return [firstNum, secondNum, data[2]];
-  } else if (firstNum <= 12 && secondNum > 12) {
+  if (firstNum <= 12 && secondNum <= 31) {
     return [secondNum, firstNum, data[2]];
-  } else if (firstNum > 12 && secondNum <= 12) {
-    return [firstNum, secondNum, data[2]];
-  } else {
-    return false;
   }
+
+  if (firstNum > 12 && secondNum <= 12) {
+    return [firstNum, secondNum, data[2]];
+  }
+
+  return false;
+};
+
+const isLeapYear = (year) => {
+  return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
+};
+
+
+const isValidDate = (day, month, year) => {
+  const daysInMonth = [31, isLeapYear(year) ? 29 : 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+  return day <= daysInMonth[month - 1];
 };
 
 const isYear = (data) => {
-  const year = Number(data[2]);
+  const year = parseInt(data[2]);
 
   if (isNaN(year)) {
     return false;
@@ -34,9 +44,9 @@ const isYear = (data) => {
   if (year > 99) {
     return [data[0], data[1], year];
   } else if (year >= 40) {
-    return [data[0], data[1], '19' + year];
+    return [data[0], data[1], 1900 + year];
   } else {
-    return [data[0], data[1], '20' + year];
+    return [data[0], data[1], 2000 + year];
   }
 };
 
@@ -62,21 +72,49 @@ const isDate = (string) => {
   }
 
   const isValidDayOrMonth = isDayOrMonth(data);
-  const isValidYear = isYear(data);
+  const isValidYear = isYear(isValidDayOrMonth || []);
 
   if (!isValidDayOrMonth || !isValidYear) {
     return false;
   }
 
-  return isValidYear;
+  const [day, month, year] = isValidYear;
+
+  if (!isValidDate(day, month, year)) {
+    return false;
+  }
+
+  return [day, month, year];
 };
 
 const parseDates = (array) => {
   return array
     .map(isDate)
     .filter(Boolean)
-    .map((el) => el.join('-'));
+    .map((el) => {
+      const day = String(el[0]).padStart(2, '0');
+      const month = String(el[1]).padStart(2, '0');
+      return `${day}-${month}-${el[2]}`;
+    });
 };
 
-const array = ['10-02-2022', 'test', '11/12/2023', '00/12/2022', '41/12/2023'];
+const array = [
+  '10-02-2022',
+  '02/10/2022',
+  's02-12-1992s',
+  '11d-12m-1985y',
+  '12-11-2023',
+  '11/12/2023',
+  '02/29/2023',
+  '02/29/2024',
+  '29-02-2024',
+  '29-02-2023',
+  '30-06-2024',
+  '06/30/2024',
+  '31-06-2024',
+  '06/31/2024',
+  '31-07-2024',
+  '07/31/2024',
+];
+
 console.log(parseDates(array));
